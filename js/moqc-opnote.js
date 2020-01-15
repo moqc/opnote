4d4c2294a136f469c4d4ca445e9c29414e6a496e
Kyle Aron <aron@soartech.com>
2020-01-15 02:13:52 -0500

    Put the extra stage information under an Info icon which will be displayed as a tooltip on hover.

--- js/moqc-opnote.js
+++ js/moqc-opnote.js
@@ -231,27 +231,3 @@ function translateName(name) {
     //replace all the underscores with spaces
     return name.replace(/_/g, ' ');
 }
-
-// https://stackoverflow.com/a/33946647
-function copyToClipboard(textToCopy) {
-    // Create a dummy input to copy the string array inside it
-    var dummy = document.createElement("textarea");
-
-    // Add it to the document
-    document.body.appendChild(dummy);
-
-    // Set its ID
-    dummy.setAttribute("id", "dummy_id");
-
-    // Output the array into it
-    document.getElementById("dummy_id").value=textToCopy;
-
-    // Select it
-    dummy.select();
-
-    // Copy its contents
-    document.execCommand("copy");
-
-    // Remove it as its not needed anymore
-    document.body.removeChild(dummy);
-}
